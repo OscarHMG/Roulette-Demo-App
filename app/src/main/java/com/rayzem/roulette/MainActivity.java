95991f6e1bb7c99909201d832da49a6de1129833
Oscar Moncayo <oscar_mg7@hotmail.com>
2019-07-19 17:20:41 -0500

    UI Bottom navigation is gone

--- app/src/main/java/com/rayzem/roulette/MainActivity.java
+++ app/src/main/java/com/rayzem/roulette/MainActivity.java
@@ -62,6 +62,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        View decorView = getWindow().getDecorView();
+        // Hide both the navigation bar and the status bar.
+        // SYSTEM_UI_FLAG_FULLSCREEN is only available on Android 4.1 and higher, but as
+        // a general rule, you should design your app to hide the status bar whenever you
+        // hide the navigation bar.
+            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+
+
         initUI();
 
         /*rouletteWheel.setOnClickListener(new View.OnClickListener() {
@@ -175,9 +185,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             if(Math.abs(event.values[2]) > ROTATION_THRESHOLD){
                 spinWheel(rouletteWheel);
             }
-
         }
-
     }
 
     @Override
